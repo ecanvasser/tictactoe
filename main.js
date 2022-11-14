1be3df6506c8d6c9c2e4789dde0133b61356a853
ecanvasser <eddiecanvassere9@gmail.com>
2022-11-14 00:53:47 -0800

    Add makeMove function to PlayerFactory

--- main.js
+++ main.js
@@ -1,32 +1,36 @@
 const Gameboard = (() => {
     
     var gameboard = [];
-
-    const renderX = () => {};
-    const renderO = () => {};
+    const board = document.querySelector('.gameboard');
+    
+    const _createBoard = (() => {
+        for (i = 0; i < 9; i++) {
+            let tile = board.appendChild(document.createElement('a'));
+            tile.setAttribute('class', 'tile');
+            tile.setAttribute('id', i)
+        }
+    })();
 
 })();
 
-const PlayerFactory = () => {
-
-    const makeMove = () => {};
+const PlayerFactory = (sym) => {
     
+    const symbol = sym;
+
+    const makeMove = (() => {
+        let tiles = document.querySelectorAll('.tile');
+        for (i = 0; i < tiles.length; i++) {
+            tiles[i].addEventListener('click', function() {
+                this.innerHTML = symbol
+            })
+        }
+    })();
+
 };
 
 const Gameflow = (() => {
 
-    const createBoard = () => {
-        const board = document.querySelector('.gameboard');
-        for (i = 0; i < 9; i++) {
-            let tile = board.appendChild(document.createElement('div'));
-            tile.setAttribute('class', 'tile');
-            tile.setAttribute('id', i)
-        }
-    };
-
-    createBoard();
-
     const moveValid = () => {};
     const winortie = () => {};
-    
+
 })();
